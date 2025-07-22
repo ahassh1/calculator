d00a4507e776de2c6f30feaf347ce5135f706099
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-23 00:01:50 +0600

    text added

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -5,7 +5,10 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function Calculator() {
   return (
     <View style={styles.container}>
-      <Text>Calculator</Text>
+      <View style={styles.display}>
+        <Text style={{fontSize:59, fontWeight:400, color:'white'}}>234354</Text>
+      </View>
+      <View style={styles.keypad}></View>
     </View>
   )
 }
@@ -14,5 +17,17 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:Colors.dark,
+    },
+    display:{
+        flex:1,
+        backgroundColor:Colors.gray,
+        paddingVertical:20,
+        paddingHorizontal:40,
+        justifyContent:'center',
+        alignItems:'flex-end'
+    },
+    keypad:{
+        flex:2,
+        backgroundColor:'green',
     }
 })
