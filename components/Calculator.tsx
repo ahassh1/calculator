94d8ed54fc67302649ad5cc51d57ff63ec515f1c
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-23 13:22:52 +0600

    handle calculation added

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,35 +1,72 @@
-import { Colors } from '@/utils/Colors'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Button from './Button'
+import { Colors } from '@/utils/Colors';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from './Button';
 
 export default function Calculator() {
+  const [firstValue, setFirstValue]=useState('');
+  const [displayValue, setDisplayValue]= useState('0');
+  const [operator, setOperator]= useState('');
+  const handleNumberInput = (num: string) => {
+  if (displayValue == '0') {
+    setDisplayValue(num);
+  } else {
+    setDisplayValue(displayValue + num);
+  }
+};
+
+const handleOperatorInput = (operator:string) =>{
+  setOperator(operator);
+  setFirstValue(displayValue);
+  setDisplayValue('0');
+}
+
+const handleCalcutation =()=>{
+  const num1= parseFloat(firstValue);
+  const num2= parseFloat(displayValue);
+
+  if(operator === '+'){
+    setDisplayValue( (num1 + num2).toString());
+  }else if(operator === '-'){
+    setDisplayValue( (num1 - num2).toString());
+  }else if(operator === '*'){
+    setDisplayValue((num1 * num2).toString());
+  }else if(operator === '/'){
+    setDisplayValue((num1 /num2).toString());
+  }else if(operator === '%'){
+    setDisplayValue((num1 % num2).toString());
+  }
+  setOperator('');
+  setFirstValue('')
+}
+
   return (
     <View style={styles.container}>
       <View style={styles.display}>
-        <Text style={{fontSize:59, fontWeight:400}}>234354</Text>
+        <Text style={{fontSize:30, fontWeight:'300'}}>{firstValue + operator}</Text>
+        <Text style={{fontSize:59, fontWeight:'400'}}>{displayValue}</Text>
       </View>
       <View style={styles.keypad}>
-     <Button title='C' type='top'/>
-     <Button title='⨂'type='top'/>
-     <Button title='%'type='top'/>
-     <Button title='÷'type='right'/>
-     <Button title='7' type='number'/>
-     <Button title='8' type='number'/>
-     <Button title='9' type='number'/>
-     <Button title='X' type='right'/>
-     <Button title='6' type='number'/>
-     <Button title='5' type='number'/>
-     <Button title='4' type='number'/>
-     <Button title='-' type='number'/>
-     <Button title='1' type='number'/>
-     <Button title='2' type='number'/>
-     <Button title='3' type='number'/>
-     <Button title='+' type='right'/>
-     <Button title='0' type='number'/>
-     <Button title='00' type='number'/>
-     <Button title='.' type='number'/>
-     <Button title='=' type='right'/>
+     <Button title='C' type='top' onPress={() => handleOperatorInput('C')}/>
+     <Button title='⨂'type='top' oonPress={() => handleOperatorInput('⨂')}/>
+     <Button title='%'type='top' oonPress={() => handleOperatorInput('%')}/>
+     <Button title='÷'type='right' onPress={() => handleOperatorInput('÷')}/>
+     <Button title='7' type='number'onPress={() => handleNumberInput('7')}/>
+     <Button title='8' type='number' onPress={() => handleNumberInput('8')}/>
+     <Button title='9' type='number' onPress={() => handleNumberInput('9')}/>
+     <Button title='X' type='right' onPress={() => handleOperatorInput('X')}/>
+     <Button title='6' type='number' onPress={() => handleNumberInput('6')}/>
+     <Button title='5' type='number' onPress={() => handleNumberInput('5')}/>
+     <Button title='4' type='number' onPress={() => handleNumberInput('4')}/>
+     <Button title='-' type='number' onPress={() => handleOperatorInput('-')}/>
+     <Button title='1' type='number' onPress={() => handleNumberInput('1')}/>
+     <Button title='2' type='number' onPress={() => handleNumberInput('2')}/>
+     <Button title='3' type='number' onPress={() => handleNumberInput('3')}/>
+     <Button title='+' type='right' onPress={() => handleOperatorInput('+')}/>
+     <Button title='0' type='number' onPress={() => handleNumberInput('0')}/>
+     <Button title='00' type='number' onPress={() => handleNumberInput('00')}/>
+     <Button title='.' type='number' onPress={() => handleNumberInput('.')}/>
+     <Button title='=' type='right' onPress={() => handleOperatorInput('=')}/>
       </View>
     </View>
   )
