fcbc5e29181e98a6238ed2236dd44240a141da06
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-23 12:16:58 +0600

    title and type added on button

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -11,6 +11,25 @@ export default function Calculator() {
       </View>
       <View style={styles.keypad}>
      <Button title='C'/>
+     <Button title='⨂'/>
+     <Button title='%'/>
+     <Button title='÷'/>
+     <Button title='7'/>
+     <Button title='8'/>
+     <Button title='9'/>
+     <Button title='X'/>
+     <Button title='6'/>
+     <Button title='5'/>
+     <Button title='4'/>
+     <Button title='-'/>
+     <Button title='1'/>
+     <Button title='2'/>
+     <Button title='3'/>
+     <Button title='+'/>
+     <Button title='0'/>
+     <Button title='00'/>
+     <Button title='.'/>
+     <Button title='='/>
       </View>
     </View>
   )
